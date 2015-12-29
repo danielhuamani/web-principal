d41abb8a04124c44449c908f35bc436489fbcd02
danielhuamani <danielhuamani15@gmail.com>
2015-12-29 18:17:17 -0500

    agrengao cambios

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,6 @@ var jade = require('gulp-jade');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
-var http = require('http');
-var st = require('st');
-var livereload = require('gulp-livereload');
 var connect = require('gulp-connect');
 var historyApiFallback = require('connect-history-api-fallback');
 var directorio = {
@@ -24,7 +21,7 @@ gulp.task('stylus', function () {
       }))
     .pipe(gulp.dest('app/static/css/'))
     .pipe(connect.reload());
-   // .pipe(livereload());
+
 });
 
 
@@ -35,21 +32,17 @@ gulp.task('templates', function() {
     .pipe(jade())
     .pipe(gulp.dest('app/templates/html'))
     .pipe(connect.reload());
-    //.pipe(livereload());
+
 });
 
 gulp.task('watch', function() {
-	//livereload.listen();
+
 	gulp.watch('app/static/stylus/styles.styl', ['stylus']),
 	gulp.watch('app/templates/jade/*.jade', ['templates'])
 });
 
 //creacioon  del server para el livereload
-gulp.task('server', function(done) {
-	http.createServer(
-		st({ path: __dirname + '/templates/html', index: 'index.html', cache: false })
-		).listen(3000, done)
-});
+
 gulp.task('connect', function() {
   connect.server({
     root: 'app/',
