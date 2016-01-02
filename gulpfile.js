881a749dfbb02683c8f8abbb96cef2556b03fabd
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2016-01-01 20:13:04 -0500

    modificando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,50 +4,55 @@ var jade = require('gulp-jade');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
-var http = require('http');
-var st = require('st');
-var livereload = require('gulp-livereload');
-
+var connect = require('gulp-connect');
+var historyApiFallback = require('connect-history-api-fallback');
 var directorio = {
-	jade: ['./templates/jade/*.jade'],
-	stylus: ['./static/stylus/styles.styl']
+  jade: ['app/templates/jade/*.jade'],
+  stylus: ['app/static/stylus/styles.styl']
 
 };
 
 
 gulp.task('stylus', function () {
   gulp.src(directorio.stylus)
-  	.pipe(plumber())
+    .pipe(plumber())
     .pipe(stylus({
       compress: true
       }))
-    .pipe(gulp.dest('./static/css/'))
-    .pipe(livereload());
+    .pipe(gulp.dest('app/static/css/'))
+    .pipe(connect.reload());
+
 });
 
 
 gulp.task('templates', function() {
 
   gulp.src(directorio.jade)
-  	.pipe(plumber())
+    .pipe(plumber())
     .pipe(jade())
-    .pipe(gulp.dest('./templates/html'))
-    .pipe(livereload());
+    .pipe(gulp.dest('app/templates/html'))
+    .pipe(connect.reload());
+
 });
 
-gulp.task('watch', ['server'], function() {
-	livereload.listen();
-	gulp.watch('./static/stylus/styles.styl', ['stylus']),
-	gulp.watch('./templates/jade/*.jade', ['templates'])
+gulp.task('watch', function() {
+
+  gulp.watch('app/static/stylus/styles.styl', ['stylus']),
+  gulp.watch('app/templates/jade/*.jade', ['templates'])
 });
 
 //creacioon  del server para el livereload
-gulp.task('server', function(done) {
-	http.createServer(
-		st({ path: __dirname + '/templates/html', index: 'index.html', cache: false })
-		).listen(3000, done)
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app/',
+    hostname: '0.0.0.0',
+    livereload: true,
+    port: '3000',
+    open: true,
+    middleware: function(connect, opt) {
+      return [ historyApiFallback() ];
+    }
+  });
 });
-
-gulp.task('default', ['stylus', 'templates', 'watch']);
-
-
+gulp.task('default', ['stylus', 'templates', 'watch', 'connect']);
